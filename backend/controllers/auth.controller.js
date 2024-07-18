d98075116a6802e9c7e3d4ea64c3c2114d9681db
musiienko25 <devmusiienko@gmail.com>
2024-07-18 17:41:31 +0300

    connected jwt token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,7 @@
+import bcrypt from "bcryptjs";
+
 import User from "../models/user.model.js";
+import generateTokenAndSetCookies from "../utils/generateToken.js";
 
 export const login = (req, res, next) => {
   console.log("login");
@@ -21,27 +24,33 @@ export const signup = async (req, res, next) => {
       return res.status(400).json({ error: "User exists" });
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashedPassword,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
       gender,
     });
 
-    console.log("Saving new user");
-    await newUser.save();
-
-    console.log("User created successfully");
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+    if (newUser) {
+      console.log("User created successfully");
+      generateTokenAndSetCookies(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "error" });
+    }
   } catch (e) {
     console.error("Error occurred:", e.message);
     res.status(500).json({ error: "Internal server error" });
